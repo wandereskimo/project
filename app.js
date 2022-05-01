2ea55d493ffaf10361d1f826b34154416963e088
Baesil <baesilshowcath@gmail.com>
2022-05-01 18:20:00 +0530

    docs- added withdrawl request

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnConnectWallet = document.querySelectorAll(".connect-nav");
+var btnConnectWalletPri = document.querySelector(".btn-primary");
+var btnNightmode = document.querySelector(".nightmode-btn");
+var btnWithdrawl = document.querySelector(".withdrawl-btn");
+var btnApprove = document.querySelector(".secondary-btn-a");
+var btnFinalize = document.querySelector(".secondary-btn-b");
